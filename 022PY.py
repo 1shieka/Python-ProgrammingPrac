631e30b85047c732839427ed13e8b6519918d77a
Vanshika Rawat <one.shika16@gmail.com>
2025-02-02 22:54:01 +0530

    Add files via upload

---
+++ 022PY.py
@@ -0,0 +1,50 @@
+#%%
+
+print("Hello Everyone !!!")
+print("Hello\nWorld")
+print("Hello\n\tWorld")
+print("' Rohit\' s date of birth is 12\\05\\1999'")
+
+
+
+# %%
+#Declare a string variable and print it
+x = "Hello"
+print(x)
+
+#%%
+#Take different data types and print values
+integer_value = 10
+float_value = 10.5
+string_value = "Python"
+boolean_value = True
+print("Integer:", integer_value)
+print("Float:", float_value)
+print("String:", string_value)
+print("Boolean:", boolean_value)
+
+#%%
+#Concatenating first name and last name
+a = "Vanshika"
+b = "Rawat"
+print("Full Name:", a + " " + b)
+
+#%%
+#Printing first name, nickname in parenthesis, and last name
+first_name = "Vanshika"
+nickname = "Vanshi"
+last_name = "Rawat"
+print(first_name, "(", nickname, ")", last_name)
+
+#%%
+#Printing formatted details
+name = "NIKUNJ BANSAL"
+sap_id = "500069944"
+dob = "13 Oct 1999"
+address = "UPES\nBidholi Campus\nPincode : 248007"
+print("NAME :", name)
+print("SAP ID :", sap_id)
+print("DATE OF BIRTH :", dob)
+print("ADDRESS :", address)
+
+# %%
